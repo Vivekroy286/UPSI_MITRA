b2e067e5f68affe433f1caf792e852d506771e20
vivek roy <vivekroy@viveks-MacBook-Air.local>
2025-09-11 13:28:30 +0530

    upgrade02.2

--- mocktest/migrations/0005_alter_question_options_and_more.py
+++ mocktest/migrations/0005_alter_question_options_and_more.py
@@ -23,7 +23,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='question',
             name='created_at',
-            field=models.DateTimeField(auto_now_add=True, default=''),
+            field=models.DateTimeField(auto_now_add=True),
             preserve_default=False,
         ),
         migrations.AddField(
